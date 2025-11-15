a06c3d8368311a6341b84f1e4eaa12afa85c0b7a
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-11-15 10:59:59 -0800

    updated for render

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://i-dream-of-jeannie.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,10 @@
 import express from "express";
-import { parse } from "path";
 import pg from "pg";
-import config from "./config.js"; //importing the connection string to our database hosted on NEON
 
 //connecting to our PostgreSQL database , or db for short
 const db = new pg.Pool({
   //new pg.Pool creates a connection to the database
-  connectionString: config.databaseUrl, //this is the credentials to access the database. Keep private
+  connectionString: process.env.DATABASE_URL, //this is the credentials to access the database. Keep private
   ssl: true, // use SSL encryption when connecting to the database to keep data safe in transit
 });
 
