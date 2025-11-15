d9a615892541fa2e79d26e9f241d9559e8f040bf
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-11-15 11:32:04 -0800

    fixed h2 for characters

--- client/src/pages/Characters.jsx
+++ client/src/pages/Characters.jsx
@@ -2,7 +2,7 @@ import Card from "../components/CharacterCard.jsx";
 export default function Characters({ characters }) {
   return (
     <>
-      <h2>Welcome to the Home Page</h2>
+      <h2>Meet I Dream Of Jeannie Characters</h2>
       <div className="card">
         {characters.map((character, index) => (
           <Card character={character} key={"character_" + index} />
