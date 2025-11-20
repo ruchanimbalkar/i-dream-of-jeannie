2fa0cebbca4668007579fff3f179848544efa38c
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-11-19 20:10:11 -0800

    fixed bugs

--- client/src/pages/Characters.jsx
+++ client/src/pages/Characters.jsx
@@ -1,11 +1,11 @@
-import Card from "../components/CharacterCard.jsx";
+import CharacterCard from "../components/CharacterCard.jsx";
 export default function Characters({ characters }) {
   return (
     <>
       <h2>Meet I Dream Of Jeannie Characters</h2>
       <div className="card">
         {characters.map((character, index) => (
-          <Card character={character} key={"character_" + index} />
+          <CharacterCard character={character} key={"character_" + index} />
         ))}
       </div>
     </>
